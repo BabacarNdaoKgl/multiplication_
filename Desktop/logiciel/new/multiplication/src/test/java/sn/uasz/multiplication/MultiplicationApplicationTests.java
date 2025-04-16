98c7dc6642e652604aae68e767f2caea59ddd37d
BabacarNdaoKgl <ndaocounda1@icloud.com>
2025-04-16 23:55:54 +0000

    first commit

---
+++ Desktop/logiciel/new/multiplication/src/test/java/sn/uasz/multiplication/MultiplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.multiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
